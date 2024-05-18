2c076ac56ea5c601d4eb4779623d295c877b86fa
eslamadel <eslamadel28398@gmail.com>
2024-05-19 02:02:05 +0300

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -8,6 +8,36 @@ resource "aws_key_pair" "my_key_pair" {
   key_name   = "my_key_pair"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCmt1ysaXC+xB+mIxztiSVfRrccVuEjc5Q1sEtmEXkH1FAhoNdC559G5HxQI05t80boY0GbUNSAJHGixKTBur27zLb4UgWyJKsNipiPZvPxA/weUN69DKZlkmJQAeOn2+LBYAXpzDJVvbb51rSwCuH+IqFSajb4qcRFXgA46zdAgzCplccdWIQ0A+K+JWrxjzCxfMhw8u1nXZU0blR7Cqwd9pMiGu2a8w9P51G6Zb70tKQrH891PP07d0cs4+YhpkbfIGwPbUpN3LkXN7PeFFE+bp9PVM+pUuUxlWtlJd9lekAT3qOLT/u/txRuGJJniqtxMdwll+ehVaEESJIRD2pgJBbiVfMFpYlN9mriRX+iebyIU8n7mXqlmSxzXZc6vYXGFr+zqMX4RMGNoIfJZ8tIxYsMSUYlP9H5lDkip+d8V3j5MScIfJjYVoOCjlmAN+MHnThvhioQ+tnNdaWxumUOI1M4py03pIBSvKj//wBCi/mgIexgNcOm1HaLDJ4/a6U= eslam@localhost.localdomain" # Replace with your public key
 }
+
+resource "aws_security_group" "public_sg" {
+  name   = "public_sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "public_sg"
+  }
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -27,6 +57,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.public_sg.id]
   key_name      = aws_key_pair.my_key_pair.key_name
 
   tags = {
