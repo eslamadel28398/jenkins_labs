808f8acccf67bec59eb2fc1c319a36b836fbf94a
eslamadel <eslamadel28398@gmail.com>
2024-05-18 23:04:32 +0300

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,11 @@ provider "aws" {
 
 }
 
-
+# Key Pair
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "my_key_pair"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCmt1ysaXC+xB+mIxztiSVfRrccVuEjc5Q1sEtmEXkH1FAhoNdC559G5HxQI05t80boY0GbUNSAJHGixKTBur27zLb4UgWyJKsNipiPZvPxA/weUN69DKZlkmJQAeOn2+LBYAXpzDJVvbb51rSwCuH+IqFSajb4qcRFXgA46zdAgzCplccdWIQ0A+K+JWrxjzCxfMhw8u1nXZU0blR7Cqwd9pMiGu2a8w9P51G6Zb70tKQrH891PP07d0cs4+YhpkbfIGwPbUpN3LkXN7PeFFE+bp9PVM+pUuUxlWtlJd9lekAT3qOLT/u/txRuGJJniqtxMdwll+ehVaEESJIRD2pgJBbiVfMFpYlN9mriRX+iebyIU8n7mXqlmSxzXZc6vYXGFr+zqMX4RMGNoIfJZ8tIxYsMSUYlP9H5lDkip+d8V3j5MScIfJjYVoOCjlmAN+MHnThvhioQ+tnNdaWxumUOI1M4py03pIBSvKj//wBCi/mgIexgNcOm1HaLDJ4/a6U= eslam@localhost.localdomain" # Replace with your public key
+}
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -23,8 +27,14 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+  key_name      = aws_key_pair.my_key_pair.key_name
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+
+output "instance_ip" {
+  value = aws_instance.web.public_ip
 }
